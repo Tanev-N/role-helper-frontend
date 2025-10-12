3f6dc11d2c11e9cef3d904bfc3d1462f90863f66
Konstantin <kostya.safronov@gmail.com>
2025-10-12 23:03:58 +0300

    Исправлена ошибка в цвтах

--- components/UI/Colors.ts
+++ components/UI/Colors.ts
@@ -5,14 +5,9 @@ export const COLORS = {
   inputBorder: "#2C2C31",
   text: "#FFFFFF",
   white: "#FFFFFF",
-<<<<<<< HEAD
   grey: "#808080",
   buttonInactive: "#rgba(73, 154, 0, 1)",
   buttonActive: "#rgba(73, 154, 0, 1)",
-=======
-  buttonInactive: "#499A00",
-  buttonActive: "#479603ff",
->>>>>>> 8adca6903a5f46a0ad36a37cd45eef1d103ff221
   link: "#FFFFFF",
   error: "#FF1D25",
 };
