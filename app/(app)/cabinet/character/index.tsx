13068b23a9d180d3638c05cb35abf365e6586fb4
n.tanev <n.tanev@vkteam.ru>
2025-11-08 16:40:38 +0300

    game store add

--- app/(app)/cabinet/character/index.tsx
+++ app/(app)/cabinet/character/index.tsx
@@ -1,33 +1,33 @@
-import React from "react";
-import { ScrollView, View, Text } from "react-native";
-import { observer } from "mobx-react-lite";
-import { characterStyles as styles } from "@/components/Character/styles";
 import CharacterMain from "@/components/Character/CharacterMain";
 import CharacterSecondary from "@/components/Character/CharacterSecondary";
+import { characterStyles as styles } from "@/components/Character/styles";
 import { COLORS } from "@/constant/colors";
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { ScrollView, Text, View } from "react-native";
 
 const CharactersScreen = () => {
-  return (
-    <ScrollView
-        style={{ flex: 1, backgroundColor: COLORS.backgroundPrimary }}
-        contentContainerStyle={{
-            alignItems: "center",
-            paddingTop: 40,
-            paddingBottom: 60,
-        }}
-    >
-        {/* === ОСНОВНАЯ ИНФОРМАЦИЯ === */}
-        <View style={styles.block}>
-            <Text style={styles.sectionTitle}>ОСНОВНАЯ ИНФОРМАЦИЯ</Text>
-            <CharacterMain />
-        </View>
+    return (
+        <ScrollView
+            style={{ flex: 1, backgroundColor: COLORS.backgroundPrimary }}
+            contentContainerStyle={{
+                alignItems: "center",
+                paddingTop: 40,
+                paddingBottom: 60,
+            }}
+        >
+            {/* === ОСНОВНАЯ ИНФОРМАЦИЯ === */}
+            <View style={styles.block}>
+                <Text style={styles.sectionTitle}>ОСНОВНАЯ ИНФОРМАЦИЯ</Text>
+                <CharacterMain />
+            </View>
 
-        {/* === ДОПОЛНИТЕЛЬНАЯ ИНФОРМАЦИЯ === */}
-        <View style={styles.block}>
-            <Text style={styles.sectionTitle}>ДОПОЛНИТЕЛЬНАЯ ИНФОРМАЦИЯ</Text>
-            <CharacterSecondary />
-        </View>
-    </ScrollView>
+            {/* === ДОПОЛНИТЕЛЬНАЯ ИНФОРМАЦИЯ === */}
+            <View style={styles.block}>
+                <Text style={styles.sectionTitle}>ДОПОЛНИТЕЛЬНАЯ ИНФОРМАЦИЯ</Text>
+                <CharacterSecondary />
+            </View>
+        </ScrollView>
     );
 }
 
--- stores/RootStore.ts
+++ stores/RootStore.ts
@@ -2,15 +2,18 @@ import { makeAutoObservable } from "mobx";
 import { AuthStore } from "./Auth/AuthStore";
 import { CharactersStore } from "./Characters/CharactersStore";
 import { SessionStore } from "./Session/SessionStore";
+import { GamesStore } from "./Games/GamesStore";
 
 export class RootStore {
   readonly authStore: AuthStore;
   readonly charactersStore: CharactersStore;
   readonly sessionStore: SessionStore;
+  readonly gamesStore: GamesStore;
   constructor() {
     makeAutoObservable(this);
     this.authStore = new AuthStore();
     this.charactersStore = new CharactersStore();
     this.sessionStore = new SessionStore();
+    this.gamesStore = new GamesStore();
   }
 }
