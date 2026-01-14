4e1dcd2a0ada2aae6450f02be25126b9cae63624
Belyak Sofya <sonyabely@gmail.com>
2026-01-14 22:19:46 +0500

    add load avatar

--- components/Cabinet/CabinetHeader.tsx
+++ components/Cabinet/CabinetHeader.tsx
@@ -28,7 +28,7 @@ export default function CabinetHeader({ authStore, router, blockWidth }: any) {
       console.log("Permission granted, launching image picker");
       
       const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaType.Images,
+        mediaTypes: ["images"],
         allowsEditing: true,
         aspect: [1, 1],
         quality: 1,
